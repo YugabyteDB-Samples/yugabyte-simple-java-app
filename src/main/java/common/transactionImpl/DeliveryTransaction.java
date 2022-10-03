68ac206e2dfb7ac44eaa460c07455f1bc132d28c
Zugl <63085208+ZuglKiki@users.noreply.github.com>
2022-10-03 16:42:43 +0800

    add delivery and related transaction

---
+++ src/main/java/common/transactionImpl/DeliveryTransaction.java
@@ -0,0 +1,29 @@
+package common.transactionImpl;
+
+import common.Transaction;
+
+import java.sql.Connection;
+
+public class DeliveryTransaction extends Transaction {
+    int W_ID;
+    int CARRIER_ID;
+    protected void actuallyExecute(Connection conn) {
+        super.actuallyExecute(conn);
+    }
+
+    public int getW_ID() {
+        return W_ID;
+    }
+
+    public void setW_ID(int w_ID) {
+        W_ID = w_ID;
+    }
+
+    public int getCARRIER_ID() {
+        return W_ID;
+    }
+
+    public void setCARRIER_ID(int carrrier_ID) {
+        CARRIER_ID = carrrier_ID;
+    }
+}
