0c14377ef2687bacc318142483de1e1bd7927db4
YANGZ001 <zyfunnyyoung@gmail.com>
2022-10-04 21:55:50 +0800

    Implement actuallyExecute for OrderStatusTransaction

--- src/main/java/common/transactionImpl/OrderStatusTransaction.java
+++ src/main/java/common/transactionImpl/OrderStatusTransaction.java
@@ -3,6 +3,9 @@ package common.transactionImpl;
 import common.Transaction;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @Package common.transactionImpl
@@ -17,7 +20,42 @@ public class OrderStatusTransaction extends Transaction {
 
     @Override
     protected void actuallyExecute(Connection conn) {
-        super.actuallyExecute(conn);
+        String sql1 = String.format("select distinct C_FIRST, C_MIDDLE, C_LAST, C_BALANCE from Customer where C_W_ID = %d, and C_D_ID = %d and C_ID = %d", C_W_ID, C_D_ID, C_ID);
+        String sql2 = String.format("select O_ID, O_ENTRY_D, O_CARRIER_ID from (select *, row_number()over(partition by O_W_ID, O_D_ID, O_C_ID order by O_ENTRY_D desc) as rank from Order where O_W_ID = %d and O_D_ID = %d and O_C_ID = %d) t where rank = 1", C_W_ID, C_D_ID, C_ID);
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql1);
+            while (rs.next()) {
+                int C_FIRST = rs.getInt(1);
+                int C_MIDDLE = rs.getInt(2);
+                int C_LAST = rs.getInt(3);
+                int C_BALANCE = rs.getInt(4);
+                System.out.printf("%d,%d,%d,%d", C_FIRST, C_MIDDLE, C_LAST, C_BALANCE);
+            }
+
+            // get O_ID
+            rs = stmt.executeQuery(sql2);
+            while (rs.next()) {
+                int O_ID = rs.getInt(1);
+                int O_ENTRY_D = rs.getInt(2);
+                int O_CARRIER_ID = rs.getInt(3);
+                // sql3
+                String sql3 = String.format("select OL_I_ID, OL_SUPPLY_W_ID, OL_QUANTITY, OL_AMOUNT, OL_DELIVERY_D from OrderLine where OL_W_ID = %d OL_D_ID = %d OL_O_ID = %d", C_W_ID, C_D_ID, O_ID);
+                ResultSet tmp = stmt.executeQuery(sql2);
+                while (tmp.next()) {
+                    int OL_I_ID = tmp.getInt(1);
+                    int OL_SUPPLY_W_ID = tmp.getInt(2);
+                    int OL_QUANTITY = tmp.getInt(3);
+                    int OL_AMOUNT = tmp.getInt(4);
+                    int OL_DELIVERY_D = tmp.getInt(5);
+                    System.out.printf("%d,%d,%d,%d,%d",OL_I_ID,OL_SUPPLY_W_ID,OL_QUANTITY,OL_AMOUNT,OL_DELIVERY_D);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (stmt == null) throw new RuntimeException("Statement is null");
     }
 
     public OrderStatusTransaction(int c_W_ID, int c_D_ID, int c_ID) {
