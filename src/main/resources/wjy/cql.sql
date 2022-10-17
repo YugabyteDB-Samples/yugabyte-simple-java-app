4bdcce948d1f5d37ae71846ba0258a1a3f5b0f39
polaris0428 <460548337@qq.com>
2022-10-17 19:50:08 +0800

    cql 4567 check v1

--- src/main/resources/wjy/cql.sql
+++ src/main/resources/wjy/cql.sql
@@ -1,7 +1,7 @@
 -- YCQL
 -- 1. New Order Transaction
--- 4. Order-Status Transaction (finished)
--- 5. Stock-Level Transaction (finished)
+-- 4. Order-Status Transaction (checked)
+-- 5. Stock-Level Transaction (checked)
 -- 6. Popular-Item Transaction (finished)
 -- 7. Top-Balance Transaction (finished)
 
@@ -9,207 +9,100 @@
 
 
 -- 4. Order-Status Transaction
-select
-    C_FIRST,
-    C_MIDDLE,
-    C_LAST,
-    C_BALANCE
-from
-    Customer
-where 
-    C_W_ID = 'C_W_ID'
-    and C_D_ID = 'C_D_ID'
-    and C_ID = 'C_ID'
-;
+-- e.g. O,1,1,1771
 
-select 
-    O_ID,
-    O_ENTRY_D,
-    O_CARRIER_ID
-from 
-    Orders
-where
-    O_W_ID = 'C_W_ID'
-    and O_D_ID = 'C_D_ID'
-    and O_C_ID = 'C_ID'
-allow filtering
-order by
-    O_ID desc
-limit 1
-;
+--CQL1
+select C_FIRST, C_MIDDLE, C_LAST, C_BALANCE from Customer
+where C_W_ID = 'C_W_ID' and C_D_ID = 'C_D_ID' and C_ID = 'C_ID';
 
+--CQL2
+select O_ID, O_ENTRY_D, O_CARRIER_ID from Orders
+where O_W_ID = 'C_W_ID' and O_D_ID = 'C_D_ID' and O_C_ID = 'C_ID'
+order by O_ID desc limit 1 allow filtering;
 -- 拿到'O_ID'
 
-select 
-    OL_I_ID,
-    OL_SUPPLY_W_ID,
-    OL_QUANTITY,
-    OL_AMOUNT,
-    OL_DELIVERY_D
-from
-    OrderLine
-where
-    OL_W_ID = 'C_W_ID'
-    OL_D_ID = 'C_D_ID'
-    OL_O_ID = 'O_ID'
-;
+--CQL3
+select OL_I_ID, OL_SUPPLY_W_ID, OL_QUANTITY, OL_AMOUNT, OL_DELIVERY_D from OrderLine
+where OL_W_ID = 'C_W_ID' and OL_D_ID = 'C_D_ID' and OL_O_ID = 'O_ID';
 
 -- 5. Stock-Level Transaction
-select 
-    D_NEXT_O_ID
-from 
-    District
-where
-    D_W_ID = 'W_ID'
-    and D_ID = 'D_ID'
+-- e.g. S,1,1,14,27
 
--- 得到 N =  D_NEXT_O_ID + 1
+--CQL1
+select D_NEXT_O_ID from District 
+where D_W_ID = 'W_ID' and D_ID = 'D_ID';
+-- 得到 N = D_NEXT_O_ID
 
-select 
-    distinct OL_I_ID
-from 
-    OrderLine
-where 
-    OL_W_ID = 'W_ID'
-    and OL_D_ID = 'D_ID'
-    and OL_O_ID >= 'N'-'L'
-    and OL_O_ID < 'N'
-allow filtering
+-- CQL2
+select OL_I_ID  from OrderLine
+where OL_W_ID = 'W_ID' and OL_D_ID = 'D_ID' and OL_O_ID >= 'N'-'L' and OL_O_ID < 'N'
+allow filtering;
+-- 得到'OL_I_ID'的集合IL (需要对'OL_I_ID'去重)
 
--- 得到最新L个订单中的item(OL_I_ID)集合IL
--- num=0; for loop (OL_I_ID in IL) 
-select 
-    S_I_ID
-from
-    Stock
-where
-    S_W_ID = 'W_ID'
-    and S_I_ID = 'OL_I_ID' --IL for loop
-    and S_QUANTITY < 'T'
-allow filtering
--- if not null: num += 1
+-- CQL3
+-- 设num = 0; 
+-- for OL_I_ID in IL:
+    select S_QUANTITY from Stock
+    where S_W_ID = 'W_ID' and S_I_ID = 'OL_I_ID'
+    allow filtering;
+    -- if S_QUANTITY < 'T', num += 1
+-- 循环结束后输出num
 
 -- 6. Popular-Item Transaction
 
-select 
-    D_NEXT_O_ID
-from 
-    District
-where
-    D_W_ID = 'W_ID'
-    and D_ID = 'D_ID'
+--CQL1
+select D_NEXT_O_ID from District 
+where D_W_ID = 'W_ID' and D_ID = 'D_ID';
+-- 得到 N = D_NEXT_O_ID
 
--- 得到 N =  D_NEXT_O_ID + 1
+--CQL2
+select O_C_ID, O_ID, O_ENTRY_D from Orders
+where O_W_ID = 'W_ID' and O_D_ID = 'D_ID' and O_ID >= 'N'-'L' and O_ID < 'N'
+allow filtering;
+-- 得到最新L个订单信息 (O_C_ID, O_ID, O_ENTRY_D)
 
-select
-    O_C_ID,
-    O_ID,
-    O_ENREY_D
-from 
-    Orders
-where
-    O_W_ID = 'W_ID'
-    and O_D_ID = 'D_ID'
-    and O_ID >= 'N'-'L'
-    and O_ID < 'N'
-allow filtering
+-- CQL3
+--for every O_C_ID:
+    select C_FIRST, C_MIDDLE, C_LAST from Customer
+    where C_W_ID = 'W_ID' and C_D_ID = 'D_ID' and C_ID = 'OL_C_ID';
+    -- 得到每个订单的用户信息 (C_FIRST, C_MIDDLE, C_LAST)
 
--- 得到最新L个订单的集合OL1(O_ID, O_C_ID)
--- 输出O_ID, O_ENREY_D
---for loop OL_C_ID in (OL1)
-select 
-    C_FIRST,
-    C_MIDDLE,
-    C_LAST
-from 
-    Customer
-where 
-    C_W_ID = 'W_ID'
-    C_D_ID = 'D_ID'
-    C_ID = 'OL_C_ID' --OL1 for loop
-;
---for loop OL1_O_ID in (OL1)
-select
-    OL_W_ID,
-    OL_D_ID,
-    OL_O_ID,
-    max(OL_QUANTITY) as MAX_OL_QUANTITY
-from 
-    OrderLine
-where 
-    OL_W_ID = 'W_ID'
-    and OL_D_ID = 'D_ID'
-    and OL_O_ID = 'OL1_O_ID' --OL1 for loop
-group by 
-    OL_W_ID,
-    OL_D_ID,
-    OL_O_ID
+--for every O_ID:
+    -- CQL4
+    select OL_W_ID, OL_D_ID, OL_O_ID, OL_QUANTITY as MAX_OL_QUANTITY from OrderLine
+    where OL_W_ID = 'W_ID' and OL_D_ID = 'D_ID' and OL_O_ID = 'O_ID'
+    order by OL_QUANTITY desc limit 1 allow filtering;
+    -- 得到 MAX_OL_QUANTITY = OL_QUANTITY
+    -- CQL5
+    select OL_W_ID, OL_D_ID, OL_O_ID, OL_I_ID
+    where OL_W_ID = 'W_ID' and OL_D_ID = 'D_ID' and OL_O_ID = 'OL_O_ID' and OL_QUANTITY = 'MAX_OL_QUANTITY'
+    allow filtering;
+    -- 得到当前订单的所有 OL_I_ID (并用另一个集合记录下所有订单的 OL_I_ID)
+    -- for every OL_I_ID:
+        -- CQL6
+        select I_NAME from Item where I_ID = 'OL_I_ID';
+        -- 输出 I_NAME, MAX_OL_QUANTITY
 
--- 得到最新L个订单的OL2(OL_O_ID, MAX_OL_QUANTITY)
---OL2 for loop (OL2_O_ID)
-select 
-    OL_W_ID,
-    OL_D_ID,
-    OL_O_ID,
-    OL_I_ID
-where 
-    OL_W_ID = 'W_ID'
-    and OL_D_ID = 'D_ID'
-    and OL_O_ID = 'OL_O_ID' --OL for loop
-    and OL_QUANTITY = 'MAX_OL_QUANTITY'
-allow filtering
---输出结果
---得到最新L个订单的popular item集合ILP(set)
--- for loop (OL_I_ID in ILP)
-select 
-    I_NAME
-from
-    Item
-where
-    I_ID = 'OL_I_ID' -- ILP for loop
-;
-
-select 
-    OL_I_ID, 
-    count(*)/'L' * 100 as I_Percentage
-from 
-    order_line_rich 
-where 
-    OL_W_ID = 'W_ID'
-    and OL_D_ID = 'D_ID'
-    and OL_O_ID >= 'N'
-    and OL_O_ID < 'N'-'L'
-    and OL_I_ID = 'OL_I_ID' -- ILP for loop
-allow filtering
+-- for every OL_I_ID:
+    -- CQL7
+    select I_NAME from Item where I_ID = 'OL_I_ID';
+    -- CQL8
+    select OL_I_ID, count(*) * 100 /'L' as I_Percentage from OrderLine
+    where OL_W_ID = 'W_ID' and OL_D_ID = 'D_ID' and OL_O_ID >= 'N'-'L' and OL_O_ID < 'N' and OL_I_ID = 'OL_I_ID'
+    group by OL_I_ID
+    allow filtering;
+    -- 输出 I_NAME, I_Percentage
 
 -- 7. Top-Balance Transaction
-select 
-    C_FIRST,
-    C_MIDDLE,
-    C_LAST,
-    C_BALANCE,
-    C_W_ID, 
-    C_D_ID
-from 
-    Customer  
-order by
-    C_BALANCE desc
-limit 10
 
--- 输出结果
--- 得到TOP10 customer的 W,D 集合set W10, D10
--- for loop (C_W_ID in W10)
-select 
-    W_NAME
-from 
-    Warehouse
-where
-    W_ID = 'C_W_ID' --(for loop)
-;
--- for loop (C_D_ID in D10)
-select 
-    D_NAME
-from 
-    District
-where
-    D_ID = 'C_D_ID' --(for loop)
+--CQL1
+select C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE, C_W_ID, C_D_ID from Customer  
+order by C_BALANCE desc limit 10;
+--得到top 10 customer的全部信息
+
+-- for every C_ID:
+    --CQL2
+    select W_NAME from Warehouse where W_ID = 'C_W_ID';
+    --CQL3
+    select D_NAME from District where D_ID = 'C_D_ID';
+    -- 输出当前customer: C_FIRST, C_MIDDLE, C_LAST, C_BALANCE, C_W_ID, C_D_ID, W_NAME, D_NAME
