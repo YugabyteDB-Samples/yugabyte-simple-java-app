99fb8ed4d505453c93e3024f0585eaf6856634ee
ZuglKiki <yluqun@gmail.com>
2022-10-19 14:29:09 +0800

    update payment配置和其他sql文件

--- src/main/java/common/transactionImpl/DeliveryTransaction.java
+++ src/main/java/common/transactionImpl/DeliveryTransaction.java
@@ -15,47 +15,52 @@ import java.util.Iterator;
 public class DeliveryTransaction extends Transaction {
     int W_ID;
     int CARRIER_ID;
-    protected void actuallyExecute(Connection conn) throws SQLException {
-        Statement stmt = conn.createStatement();
-        // 获取到每个district对应的最小order_number
-        ResultSet rs = stmt.executeQuery(String.format("select " +
-                "t1.O_W_ID, t1.O_D_ID, t1.O_ID, t1.O_C_ID, t2.SUM_AMT " +
-                "from(" +
-                "select " +
-                "O_W_ID, O_D_ID, O_ID, O_C_ID," +
-                "rank() over(partition by O_W_ID,O_D_ID order by O_ID ASC) as rnk " +
-                "FROM Orders WHERE O_W_ID=%d and O_CARRIER_ID is null" +
-                ")t1 " +
-                "left join " +
-                "(SELECT " +
-                "OL_W_ID, OL_D_ID, OL_O_ID," +
-                "SUM(OL_AMOUNT) AS SUM_AMT " +
-                "FROM " +
-                "orderline " +
-                "GROUP BY " +
-                "OL_W_ID, OL_D_ID, OL_O_ID" +
-                ")t2 " +
-                "on t1.O_W_ID=t2.OL_W_ID AND t1.O_D_ID=t2.OL_D_ID AND t1.O_ID=t2.OL_O_ID " +
-                "where t1.rnk=1", W_ID));
-        int[][] tmpList = new int[10][];
-        int index = 0;
-        while (rs.next()) {
-            System.out.println("存参数中...");
-            tmpList[index] = new int[4];
-            tmpList[index][0] = rs.getInt(1);
-            tmpList[index][1] = rs.getInt(2);
-            tmpList[index][2] = rs.getInt(3);
-            tmpList[index][3] = rs.getInt(4);
-            index++;
+    protected void YSQLExecute(Connection conn) {
+        try {
+            Statement stmt = conn.createStatement();
+            // 获取到每个district对应的最小order_number
+            ResultSet rs = stmt.executeQuery(String.format("select " +
+                    "t1.O_W_ID, t1.O_D_ID, t1.O_ID, t1.O_C_ID, t2.SUM_AMT " +
+                    "from(" +
+                    "select " +
+                    "O_W_ID, O_D_ID, O_ID, O_C_ID," +
+                    "rank() over(partition by O_W_ID,O_D_ID order by O_ID ASC) as rnk " +
+                    "FROM Orders WHERE O_W_ID=%d and O_CARRIER_ID is null" +
+                    ")t1 " +
+                    "left join " +
+                    "(SELECT " +
+                    "OL_W_ID, OL_D_ID, OL_O_ID," +
+                    "SUM(OL_AMOUNT) AS SUM_AMT " +
+                    "FROM " +
+                    "orderline " +
+                    "GROUP BY " +
+                    "OL_W_ID, OL_D_ID, OL_O_ID" +
+                    ")t2 " +
+                    "on t1.O_W_ID=t2.OL_W_ID AND t1.O_D_ID=t2.OL_D_ID AND t1.O_ID=t2.OL_O_ID " +
+                    "where t1.rnk=1", W_ID));
+            int[][] tmpList = new int[10][5];
+            int index = 0;
+            while (rs.next()) {
+                System.out.println("存参数中...");
+                tmpList[index][0] = rs.getInt(1);
+                tmpList[index][1] = rs.getInt(2);
+                tmpList[index][2] = rs.getInt(3);
+                tmpList[index][3] = rs.getInt(4);
+                tmpList[index][4] = rs.getInt(5);
+                index++;
+            }
+            System.out.println("Delivery Transaction正在执行中...");
+            // 写一个for循环
+            for (int i = 0; i < 10; i++) {
+                stmt.execute((String.format("UPDATE Orders SET O_CARRIER_ID=%d WHERE O_W_ID=%d and O_D_ID=%d and O_ID=%d", CARRIER_ID, tmpList[i][0], tmpList[i][1], tmpList[i][2])));
+                stmt.execute(String.format("UPDATE OrderLine SET OL_DELIVERY_D=(SELECT CURRENT_TIMESTAMP) WHERE OL_W_ID=%d and OL_D_ID=%d and OL_O_ID=%d", tmpList[i][0], tmpList[i][1], tmpList[i][2]));
+                stmt.execute(String.format("UPDATE Customer SET C_BALANCE=C_BALANCE+%d WHERE C_W_ID=%d and C_D_ID=%d and C_ID=%d", tmpList[i][4], tmpList[i][0], tmpList[i][1], tmpList[i][3]));
+                stmt.execute(String.format("UPDATE Customer SET C_DELIVERY_CNT=C_DELIVERY_CNT+%d WHERE C_W_ID=%d and C_D_ID=%d and C_ID=%d", 1, tmpList[i][0], tmpList[i][1], tmpList[i][2]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println("Delivery Transaction正在执行中...");
-        // 写一个for循环
-        for (int i = 0; i < 10; i++) {
-            stmt.execute((String.format("UPDATE Orders SET O_CARRIER_ID=%d WHERE O_W_ID=%d and O_D_ID=%d and O_ID=%d", CARRIER_ID, tmpList[i][0], tmpList[i][1], tmpList[i][2])));
-            stmt.execute(String.format("UPDATE OrderLine SET OL_DELIVERY_D=(SELECT CURRENT_TIMESTAMP) WHERE OL_W_ID=%d and OL_D_ID=%d and OL_O_ID=%d", tmpList[i][0], tmpList[i][1], tmpList[i][2]));
-            stmt.execute(String.format("UPDATE Customer SET C_BALANCE=C_BALANCE+%d WHERE C_W_ID=%d and C_D_ID=%d and C_ID=%d", tmpList[i][4], tmpList[i][0], tmpList[i][1], tmpList[i][3]));
-            stmt.execute(String.format("UPDATE Customer SET C_DELIVERY_CNT=C_DELIVERY_CNT+%d WHERE C_W_ID=%d and C_D_ID=%d and C_ID=%d", 1, tmpList[i][0], tmpList[i][1], tmpList[i][2]));
-        }
+
     }
 
 
