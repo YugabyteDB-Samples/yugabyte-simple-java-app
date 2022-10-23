a88b1e938fd2a206034c3694e174deea6fff7480
YANGZ001 <zyfunnyyoung@gmail.com>
2022-10-23 17:06:46 +0800

    Feature: Refactor ExecuteManager to Allow ordered statistics.

--- src/main/java/ExecuteManager.java
+++ src/main/java/ExecuteManager.java
@@ -14,47 +14,50 @@ import java.util.*;
  */
 public class ExecuteManager {
     private Set<TransactionType> skipSet;
-    private List<TransactionType> transactionTypeList;
-    private Map<TransactionType, Statistics> map;
+    private List<Statistics> transactionTypeList;
 
     public ExecuteManager() {
         transactionTypeList = new ArrayList<>(8);
-        map = new HashMap<>(8);
         skipSet = new HashSet<>(8);
-
         System.out.println(TransactionType.values());
-        for (TransactionType transactionType : TransactionType.values()) {
-            map.put(transactionType, new Statistics(transactionType));
-        }
-//        skipSet.add(TransactionType.DELIVERY); // 7 minutes
-        skipSet.addAll(Arrays.asList(TransactionType.values()));
-        skipSet.remove(TransactionType.POPULAR_ITEM);
+        transactionTypeList.add(new Statistics(TransactionType.NEW_ORDER));
+        transactionTypeList.add(new Statistics(TransactionType.PAYMENT));
+        transactionTypeList.add(new Statistics(TransactionType.DELIVERY));
+        transactionTypeList.add(new Statistics(TransactionType.ORDER_STATUS));
+        transactionTypeList.add(new Statistics(TransactionType.STOCK_LEVEL));
+        transactionTypeList.add(new Statistics(TransactionType.POPULAR_ITEM));
+        transactionTypeList.add(new Statistics(TransactionType.TOP_BALANCE));
+        transactionTypeList.add(new Statistics(TransactionType.RELATED_CUSTOMER));
+
+        skipSet.add(TransactionType.NEW_ORDER);
+        skipSet.add(TransactionType.DELIVERY);
+        skipSet.add(TransactionType.RELATED_CUSTOMER);
     }
 
     public void executeYSQL(Connection conn, List<Transaction> list) throws SQLException {
         System.out.printf("Execute YSQL transactions\n");
-//        for (Transaction transaction : list) {
-//            if (skipSet.contains(transaction.getTransactionType())) continue;
-//            long executionTime = transaction.executeYSQL(conn);
-//            map.get(transaction.getTransactionType()).addNewData(executionTime);
-//            report();
-//        }
+        for (Transaction transaction : list) {
+            if (skipSet.contains(transaction.getTransactionType())) continue;
+            long executionTime = transaction.executeYSQL(conn);
+            transactionTypeList.get(transaction.getTransactionType().index).addNewData(executionTime);
+            report();
+        }
     }
 
     public void executeYCQL(CqlSession session, List<Transaction> list) {
         System.out.printf("Execute YCQL transactions\n");
-//        for (Transaction transaction : list) {
-//            if (skipSet.contains(transaction.getTransactionType())) continue;
-//            long executionTime = transaction.executeYCQL(session);
-//            map.get(transaction.getTransactionType()).addNewData(executionTime);
-//            report();
-//        }
+        for (Transaction transaction : list) {
+            if (skipSet.contains(transaction.getTransactionType())) continue;
+            long executionTime = transaction.executeYCQL(session);
+            transactionTypeList.get(transaction.getTransactionType().index).addNewData(executionTime);
+            report();
+        }
     }
 
     public void report() {
         System.out.println("---Statistics start---");
-        for (Map.Entry<TransactionType, Statistics> entry : map.entrySet()) {
-            System.out.println(entry.getValue());
+        for (Statistics statistics : transactionTypeList) {
+            System.out.println(statistics);
         }
         System.out.println("---Statistics end---");
     }
