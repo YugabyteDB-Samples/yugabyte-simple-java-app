5fde254e746f3ff55a65f8eb9859c67097a51bce
YANGZ001 <zyfunnyyoung@gmail.com>
2022-10-25 22:57:21 +0800

    Fix: test

--- src/main/java/ExecuteManager.java
+++ src/main/java/ExecuteManager.java
@@ -1,6 +1,7 @@
 import com.datastax.oss.driver.api.core.CqlSession;
 import common.Transaction;
 import common.TransactionType;
+import jnr.ffi.annotations.In;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -15,11 +16,14 @@ import java.util.*;
 public class ExecuteManager {
     private Set<TransactionType> skipSet;
     private List<Statistics> transactionTypeList;
+    private Map<TransactionType, Integer> skipMap;
 
     public ExecuteManager() {
         transactionTypeList = new ArrayList<>(8);
         skipSet = new HashSet<>(8);
-        System.out.println(TransactionType.values());
+        skipMap = new HashMap<>(8);
+
+//        System.out.println(TransactionType.values());
         transactionTypeList.add(new Statistics(TransactionType.NEW_ORDER));
         transactionTypeList.add(new Statistics(TransactionType.PAYMENT));
         transactionTypeList.add(new Statistics(TransactionType.DELIVERY));
@@ -29,20 +33,27 @@ public class ExecuteManager {
         transactionTypeList.add(new Statistics(TransactionType.TOP_BALANCE));
         transactionTypeList.add(new Statistics(TransactionType.RELATED_CUSTOMER));
 
+        for (TransactionType transactionType : TransactionType.values()) {
+            skipMap.put(transactionType, 0);
+        }
+
         // 正选逻辑
 //        skipSet.add(TransactionType.NEW_ORDER);
 //        skipSet.add(TransactionType.DELIVERY);
 //        skipSet.add(TransactionType.RELATED_CUSTOMER);
 
         // 反选逻辑
-        skipSet.addAll(Arrays.asList(TransactionType.values()));
-        skipSet.remove(TransactionType.NEW_ORDER);
+//        skipSet.addAll(Arrays.asList(TransactionType.values()));
+//        skipSet.remove(TransactionType.NEW_ORDER);
     }
 
     public void executeYSQL(Connection conn, List<Transaction> list) throws SQLException {
         System.out.printf("Execute YSQL transactions\n");
         for (Transaction transaction : list) {
             if (skipSet.contains(transaction.getTransactionType())) continue;
+            int cnt = skipMap.get(transaction.getTransactionType());
+            if (cnt >= 100) continue;
+            skipMap.put(transaction.getTransactionType(), cnt+1);
             long executionTime = transaction.executeYSQL(conn);
             transactionTypeList.get(transaction.getTransactionType().index).addNewData(executionTime);
             report();
--- src/main/java/SampleApp.java
+++ src/main/java/SampleApp.java
@@ -72,7 +72,6 @@ public class SampleApp {
             String[] firstLine = scanner.nextLine().split(",");
             String type = firstLine[0];
             Transaction transaction = null;
-            if (!type.equals(TransactionType.RELATED_CUSTOMER.type)) continue;
             if (type.equals(TransactionType.PAYMENT.type)) {
                 transaction = assemblePaymentTransaction(firstLine, scanner);
             } else if (type.equals(TransactionType.DELIVERY.type)) {
